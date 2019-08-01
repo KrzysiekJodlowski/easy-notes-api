e5962a8187c267bee7111216362650da53f19353
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-08-01 11:12:27 +0200

    Initialize Spring project with Spring Initializr Add .gitignore file

---
+++ src/main/java/com/jodlowski/easynotesapi/EasyNotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jodlowski.easynotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jodlowski/easynotesapi/EasyNotesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jodlowski.easynotesapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyNotesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
