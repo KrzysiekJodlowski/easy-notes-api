0d1c84065ca3de7a4968cba751fa5d66383778d2
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-08-01 11:59:45 +0200

    Create logger object in main class

--- src/main/java/com/jodlowski/easynotesapi/EasyNotesApiApplication.java
+++ src/main/java/com/jodlowski/easynotesapi/EasyNotesApiApplication.java
@@ -1,12 +1,16 @@
 package com.jodlowski.easynotesapi;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasyNotesApiApplication {
 
+	public static Logger logger = Logger.getLogger(EasyNotesApiApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Easy Notes application starting");
 		SpringApplication.run(EasyNotesApiApplication.class, args);
 	}
 
